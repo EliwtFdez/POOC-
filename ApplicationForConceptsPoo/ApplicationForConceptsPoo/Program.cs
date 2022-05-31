af16a7c8feabcdb93fe8c2f69bb8322fa53d024e
CRISTO ELIW ROSALES FERNANDEZ <L21020157@veracruz.tecnm.mx>
2022-05-31 00:02:51 -0500

    Class Day Ended With Exceptions

---
+++ ApplicationForConceptsPoo/ApplicationForConceptsPoo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ApplicationForConceptsPoo;
+
+Console.WriteLine("Hello, Mr Eliwt!");
+Console.WriteLine("======================");
+
+try
+{
+    Console.WriteLine(new Date(2002,3,22));
+    Console.WriteLine(new Date(2032,11,30));
+    Console.WriteLine(new Date(2022, 5, 45));
+}
+catch (Exception ERROR)
+{
+
+    Console.WriteLine(ERROR.Message);
+}
